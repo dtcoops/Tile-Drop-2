19a9109867a39cecc54606a3356ed6d9610b5d80
dtcoops <danielcooper179@gmail.com>
2022-12-21 20:48:36 -0800

    Initial commit

---
+++ Assets/Tile.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    int touchCountTotal;
+    int touchCountCurrent;
+    int fallDelayTime;
+    bool isFalling;
+    float minimumYValue; // Cannot fall below
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void BeginFall()
+    {
+
+    }
+
+    void Destroy()
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TileDrop
+ 
