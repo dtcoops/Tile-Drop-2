47792a7c7de5cf784e9c1af543f00312880746bb
dtcoops <danielcooper179@gmail.com>
2025-08-21 13:03:36 -0700

    implement tile count

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private List<Transform> patrolPoints;
+    [SerializeField]
+    private float speed;
+
+    private float positionMarginOfError;
+    private int lastVisitedIndex;
+    private int targetLocationIndex;
+
     void Start()
     {
-        
+        positionMarginOfError = .05f;
+        lastVisitedIndex = 0;
+        targetLocationIndex = 1;
+
+        Transform enemy = GameObject.FindWithTag("Enemy").transform;
+        Debug.Log(enemy, enemy);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    public void Move()
+    {
+        Debug.Log(Vector3.Distance(this.transform.position, patrolPoints[targetLocationIndex].position));
+        Debug.Log(positionMarginOfError);
+        if (Vector3.Distance(gameObject.transform.position, patrolPoints[targetLocationIndex].position) <= positionMarginOfError)
+        {
+            targetLocationIndex++;
+            lastVisitedIndex++;
+            if (targetLocationIndex >= patrolPoints.Count)
+            {
+                targetLocationIndex = 0;
+            }
+            if (lastVisitedIndex >= patrolPoints.Count)
+            {
+                lastVisitedIndex = 0;
+            }
+        }
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, patrolPoints[targetLocationIndex].position, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            // Explode --- explosion animation? -- destroy block on same Y coordinate in a radius
+        }
+    }
+
+    void Explode()
+    {
+        GetComponent<SphereCollider>().radius = 3;
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,40 +4,58 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    [SerializeField]
-    private int touchCountTotal;
-    [SerializeField]
-    private int touchCountCurrent;
-    [SerializeField]
-    private float fallDelayTime;
-    [SerializeField]
-    private bool isFalling;
-    [SerializeField]
-    private float minimumYValue; // Cannot fall below
+    [SerializeField] private int touchCountTotal;
+    [SerializeField] private int touchCountCurrent;
+    [SerializeField] private float fallDelayTime;
+    [SerializeField] private bool isFalling;
+    [SerializeField] private float minimumYValue; // Cannot fall below
 
+    private bool timerStarted = false;
+    private Rigidbody rb;
+
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+
+        touchCountTotal = touchCountTotal != 0 ? touchCountTotal : 1;
+        touchCountCurrent = touchCountCurrent != 0 ? touchCountCurrent : 0;
+    }
     private void Update()
     {
         if (isFalling)
         {
-            if(transform.position.y <= minimumYValue)
+            if (transform.position.y <= minimumYValue)
             {
-                Destroy();
+                Destroy(gameObject);
             }
         }
     }
-    void BeginFall()
+
+    private void OnCollisionEnter(Collision collision)
     {
-        isFalling = true;
-        Invoke("Fall", fallDelayTime);
+        if (!collision.gameObject.CompareTag("Player")) return;
+        if (timerStarted) return;
+
+        touchCountCurrent++;
+
+        if (!timerStarted && touchCountCurrent >= touchCountTotal)
+        {
+            timerStarted = true;
+            isFalling = true;
+            Invoke(nameof(Fall), fallDelayTime);
+        }
+        
+        return;
     }
 
     private void Fall()
     {
+        GetComponent<Rigidbody>().constraints =
+         RigidbodyConstraints.FreezePositionZ |
+         RigidbodyConstraints.FreezePositionX |
+         RigidbodyConstraints.FreezeRotation;
         GetComponent<Rigidbody>().useGravity = true;
     }
-
-    void Destroy()
-    {
-
-    }
+    
 }
